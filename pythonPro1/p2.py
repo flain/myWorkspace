b187e4e088df57ca4f64a0a6ccc53aafab49d87b
FLAIN <pengfeiliu@qq.com>
2015-07-02 13:40:04 +0800

    add
    add

--- pythonPro1/p2.py
+++ pythonPro1/p2.py
@@ -2,3 +2,15 @@ __author__ = 'adminweke'
 a = 1
 b = 2
 print a + b
+
+class animal:
+    def __init__(self):
+        print 'in animal init'
+    def run(self):
+        print 'animal run'
+a=animal()
+a.run()
+class dog(animal):
+    pass
+d=dog()
+d.run()
